457f7e143e66293dd45f6195b7c3afb96f958b95
sitroq1 <sitroq@gmail.com>
2023-05-18 17:15:48 +0300

    feat: moving the main context menu

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -4,7 +4,7 @@ import PlaylistCover from "./PlaylistCover";
 import PlaylistDescription from "./PlaylistDescription";
 import PlaylistTitle from "./PlaylistTitle";
 import PlaylistContexMenu from "./PlaylistContexMenu";
-import { useState, useRef, useLayoutEffect } from "react";
+import { useState, useRef, useLayoutEffect, useEffect } from "react";
 
 const menuItems = [
     {
@@ -45,9 +45,28 @@ export default function Playlist({coverUrl, title, description, classes, toggleS
     
     const bgClasses = isContexMenuOpen ? 'bg-[#272727]' : 'bg-[#181818] hover:bg-[#272727]';
 
+    function updateContexMenuHorizontalPosition() {
+
+        const menuWidth = contexMenuRef.current.offsetWidth;
+        const shouldMoveLeft = menuWidth > window.innerWidth - clickPosition.x;
+
+
+        contexMenuRef.current.style.left = shouldMoveLeft ? `${clickPosition.x - menuWidth}px` : `${clickPosition.x}px`;
+
+    }
+
+    function updateContexMenuVerticalPosition() {
+        const menuHeight = contexMenuRef.current.offsetHeight;
+        const shouldMoveUp = menuHeight > window.innerHeight - clickPosition.y;
+
+
+        contexMenuRef.current.style.top = shouldMoveUp ? `${clickPosition.y - menuHeight}px` : `${clickPosition.y}px`;
+    }
+
     function updateContexMenuPosition() {
-        contexMenuRef.current.style.top = `${clickPosition.y}px`;
-        contexMenuRef.current.style.left = `${clickPosition.x}px`;
+        
+        updateContexMenuHorizontalPosition();
+        updateContexMenuVerticalPosition();
 
     }
 
@@ -59,6 +78,33 @@ export default function Playlist({coverUrl, title, description, classes, toggleS
             updateContexMenuPosition();
         }
 
+    })
+
+    useEffect(() => {
+
+        if (!isContexMenuOpen) return;
+
+        function handleClickAway(event) {
+            if (!contexMenuRef.current.contains(event.target)) {
+                onCloseContextMenu();
+            }
+
+        }
+
+        function handleEsc(event) {
+            if(event.keyCode === 27) {
+                onCloseContextMenu();
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickAway);
+        document.addEventListener('keydown', handleEsc);
+        
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickAway);
+            document.removeEventListener('keydown', handleClickAway);
+        }
     })
 
     function OpenContexMenu (event) {
@@ -87,7 +133,7 @@ export default function Playlist({coverUrl, title, description, classes, toggleS
                 <PlaylistTitle title={title}/>
                 <PlaylistDescription description={description} />
                 {isContexMenuOpen && (
-                    <PlaylistContexMenu menuItems={menuItems} ref={contexMenuRef} onClose={onCloseContextMenu} classes="fixed bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10"/>
+                    <PlaylistContexMenu menuItems={menuItems} ref={contexMenuRef} classes="fixed bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10"/>
                 )}
                 
             </a>
--- src/components/PlaylistContexMenu.jsx
+++ src/components/PlaylistContexMenu.jsx
@@ -1,39 +1,14 @@
-import React, {useLayoutEffect, useEffect} from "react";
+import React from "react";
 
 import PlaylistContexMenuItem from "./PlaylistContexMenuItem";
 
 
 
- function PlaylistContexMenu({menuItems, classes, onClose: handleClose}, ref) {
+ function PlaylistContexMenu({menuItems, classes}, ref) {
 
    
 
-    useLayoutEffect(() => {
-
-        if (!handleClose) return;
-
-        function handleClickAway(event) {
-            if (!ref.current.contains(event.target)) {
-                handleClose();
-            }
-
-        }
-
-        function handleEsc(event) {
-            if(event.keyCode === 27) {
-                handleClose();
-            }
-        }
-
-        document.addEventListener('mousedown', handleClickAway);
-        document.addEventListener('keydown', handleEsc);
-        
-
-        return () => {
-            document.removeEventListener('mousedown', handleClickAway);
-            document.removeEventListener('keydown', handleClickAway);
-        }
-    })
+    
     
     
 
