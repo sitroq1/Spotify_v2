cb0480653747c8553b9ea66a5a723c7c35e25125
sitroq1 <sitroq@gmail.com>
2023-05-19 21:53:40 +0300

    feat: change subMenuItems position

--- src/components/PlaylistContexMenuItem.jsx
+++ src/components/PlaylistContexMenuItem.jsx
@@ -1,28 +1,61 @@
 
 
-import {ChevronRightIcon} from '@heroicons/react/24/outline'
+import { ChevronRightIcon } from '@heroicons/react/24/outline'
 import PlaylistContexMenu from './PlaylistContexMenu'
+import { useRef, useState } from 'react'
 
-export default function PlaylistContexMenuItem({children: label, subMenuItems}) {
+export default function PlaylistContexMenuItem({ children: label, subMenuItems }) {
 
 
-    if(subMenuItems) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [subContexMenuClasses, setSubContexMenuClasses] = useState('left-full');
+    const menuItemRef = useRef(null);
+
+    function toContexMenuClasses() {
+        
+        const menuItem = menuItemRef.current;
+        const menuItemWidth = menuItem.offsetWidth;
+        const windowWidth = window.innerWidth;
+        const menuItemEndCoordinate = menuItem.getBoundingClientRect().right;
+        const shouldMoveLeft = menuItemWidth > windowWidth - menuItemEndCoordinate;
+
+        return shouldMoveLeft ? 'right-full': 'left-full';
+        
+    }
+
+    function openMenu() {
+        setIsMenuOpen(true)
+
+        setSubContexMenuClasses(toContexMenuClasses())
+
+
+    }
+
+    function closeMenu() {
+        setIsMenuOpen(false)
+    }
+
+
+    if (subMenuItems) {
 
         return (
             <>
-                <li className='relative'>
-                    <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center peer">
-                        {label} <ChevronRightIcon className='h-4 w-4'/>
+                <li className='relative' onMouseLeave={closeMenu} onMouseOver={openMenu}>
+                    <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center">
+                        {label} <ChevronRightIcon className='h-4 w-4' />
                     </button>
-                    <PlaylistContexMenu menuItems={subMenuItems} classes="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default invisible peer-hover:visible hover:visible "/>
+                    {isMenuOpen &&
+                        <PlaylistContexMenu menuItems={subMenuItems} ref={menuItemRef} classes={`absolute top-0 bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default ${subContexMenuClasses}`} />
+                    }
+
                 </li>
             </>
         )
 
-        
+
 
     }
-    
+
 
 
     return (
